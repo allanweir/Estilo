6df4313c5023ba58a41f6fc92128bb611088f84c
Allan Weir <allan.weir@me.com>
2018-02-28 11:27:18 +0000

    Initial Commit

---
+++ Estilo/Estilo.h
@@ -0,0 +1,19 @@
+//
+//  Estilo.h
+//  Estilo
+//
+//  Created by Allan Weir on 28/02/2018.
+//  Copyright © 2018 Studious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Estilo.
+FOUNDATION_EXPORT double EstiloVersionNumber;
+
+//! Project version string for Estilo.
+FOUNDATION_EXPORT const unsigned char EstiloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Estilo/PublicHeader.h>
+
+
